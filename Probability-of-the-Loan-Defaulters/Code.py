bff1659d9181cc172b2c8c080020573a54cabf43
Amanjyot Singh <40793658+Amanjyot62@users.noreply.github.com>
2020-05-16 22:26:38 +0530

    Update and rename code.py to Code.py

--- Probability-of-the-Loan-Defaulters/Code.py
+++ Probability-of-the-Loan-Defaulters/Code.py
@@ -1,4 +1,3 @@
-# --------------
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -25,9 +24,6 @@ print(p_a_b)
 # Check whether the P(A) and P(B) are independent from each other
 result = (p_a == p_a_b)
 print(result)
-
-
-# --------------
 # probability of paid_back_loan is Yes
 prob_lp = df[df['paid.back.loan'] == 'Yes'].shape[0] / df.shape[0]
 print(prob_lp)
@@ -49,25 +45,35 @@ bayes = (prob_pd_cs * prob_lp)/ prob_cs
 
 # print bayes
 print(bayes)
+# create bar plot for purpose
+df.purpose.value_counts(normalize=True).plot(kind='bar')
+plt.title("Probability Distribution of Purpose")
+plt.ylabel("Probability")
+plt.xlabel("Number of Purpose")
+plt.show()
 
+#create new dataframe for paid.back.loan == 'No'
+df1= df[df['paid.back.loan'] == 'No']
 
-# --------------
-# code starts here
-df1=df[df['paid.back.loan'] == 'No']
-#print(df1.head(5))
+# plot the bar plot for 'purpose' where paid.back.loan == No 
 df1.purpose.value_counts(normalize=True).plot(kind='bar')
-# code ends here
+plt.title("Probability Distribution of Purpose")
+plt.ylabel("Probability")
+plt.xlabel("Number of Purpose")
+plt.show()
+
+# Calculate median 
+inst_median = df['installment'].median()
+inst_mean = df['installment'].mean()
 
 
-# --------------
-# code starts here
-inst_median=df['installment'].median()
-inst_mean=df['installment'].mean()
-
+# histogram for installment
 df['installment'].hist(normed = True, bins=50)
+plt.axvline(x=inst_median,color='r')
+plt.axvline(x=inst_mean,color='g')
 
+plt.show()
+
+#histogram for log anual income
 df['log.annual.inc'].hist(normed = True, bins=50)
-
-# code ends here
-
-
+plt.show()
