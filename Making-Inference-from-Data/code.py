44995d2d55be89bf05246956e258bb8cc0533eaa
Amanjyot Singh <40793658+Amanjyot62@users.noreply.github.com>
2020-05-17 11:29:22 +0530

    Update code.py

--- Making-Inference-from-Data/code.py
+++ Making-Inference-from-Data/code.py
@@ -1,4 +1,3 @@
-# --------------
 import pandas as pd
 import scipy.stats as stats
 import math
@@ -8,31 +7,47 @@ import warnings
 warnings.filterwarnings('ignore')
 #Sample_Size
 sample_size=2000
-data=pd.read_csv(path,sep=',')
+
 #Z_Critical Score
 z_critical = stats.norm.ppf(q = 0.95)  
-data_sample=data.sample(n=sample_size,random_state=0)
-sample_mean=data_sample['installment'].mean()
-sample_std=data_sample['installment'].std()
 
-margin_of_error=z_critical * (sample_std/math.sqrt(sample_size))
-confidence_interval=(sample_mean - margin_of_error, sample_mean + margin_of_error)
 # path        [File location variable]
+
+
+#Reading file
+data=pd.read_csv(path)
+
+#Sampling the dataframe
+data_sample = data.sample(n=sample_size, random_state=0)
+
+
+#Finding the mean of the sample
+sample_mean = data_sample['installment'].mean()
+
+#Finding the standard deviation of the sample
+sample_std = data_sample['installment'].std()
+
+#Finding the margin of error
+margin_of_error = z_critical * (sample_std/math.sqrt(sample_size))
+
+#Finding the confidence interval
+confidence_interval = (sample_mean - margin_of_error,
+                       sample_mean + margin_of_error)  
+
+print("Confidence interval:")
+print(confidence_interval)
+
+#Finding the true mean
 true_mean=data['installment'].mean()
-print(true_mean)
 
-#Code starts here
+print(("True mean: {}".format(true_mean)))
 
-
-
-# --------------
 import matplotlib.pyplot as plt
 import numpy as np
 
 #Different sample sizes to take
 sample_size=np.array([20,50,100])
 
-#Code starts here
 
 #Creating different subplots
 fig,axes=plt.subplots(3,1, figsize=(10,20))
@@ -63,44 +78,62 @@ for i in range(len(sample_size)):
 
 #Displaying the plot
 plt.show()
-
-
-#Code ends here
-
-
-# --------------
 #Importing header files
 
 from statsmodels.stats.weightstats import ztest
-x=data['int.rate'].map(lambda x: str(x)[:-1])
 
-x=x.astype(float)/100
-#print(x)
-data['int.rate']=x
 
-z_statistic, p_value = ztest(data[data['purpose']=='small_business']['int.rate'],value=data['int.rate'].mean(),alternative='larger')
-if p_value<0.05:
-    print('Reject')
-else:
-    print("Accept")
-#Code starts here
+# Removing the last character from the values in column
+data['int.rate'] = data['int.rate'].map(lambda x: str(x)[:-1])
+
+#Dividing the column values by 100
+data['int.rate']=data['int.rate'].astype(float)/100
 
 
 
-# --------------
+#Applying ztest for the hypothesis
+z_statistic, p_value = ztest(x1=data[data['purpose']=='small_business']['int.rate'], value=data['int.rate'].mean(), alternative='larger')
+
+print(('Z-statistic is :{}'.format(z_statistic)))
+print(('P-value is :{}'.format(p_value)))
+"""The bank thinks that monthly installments (installment) customers have to pay might have some sort of effect on loan defaulters
+
+Let's do hypothesis testing(two-sided) on that
+
+Null Hypothesis H_0: \mu $(yes) == $\muH 
+0
+​	
+ :μ¨D(yes)==¨Dμ(no)
+
+Meaning: There is no difference in installments being paid by loan defaulters and loan non defaulters
+
+Alternate Hypothesis H_1: \mu $(yes) $ \neq \muH 
+1
+​	
+ :μ¨D(yes)¨D̸=μ(no)
+
+Meaning: There is difference in installments being paid by loan defaulters and loan non defaulters"""
+
 #Importing header files
 from statsmodels.stats.weightstats import ztest
-z_statistic,p_value=ztest(data[data['paid.back.loan']=='No']['installment'],data[data['paid.back.loan']=='Yes']['installment'])
 
-if p_value<0.05:
-    print("reject")
-else:
-    print("Accept")    
-#Code starts here
+#Applying ztest for the hypothesis
+z_statistic, p_value = ztest(x1=data[data['paid.back.loan']=='No']['installment'], x2=data[data['paid.back.loan']=='Yes']['installment'])
 
+print(('Z-statistic is :{}'.format(z_statistic)))
+print(('P-value is :{}'.format(p_value)))
 
 
-# --------------
+"""Purpose vs Loan Defaulting
+Another thing bank suspects is that there is a strong association between purpose of the loan(purpose column) of a person and whether that person has paid back loan (paid.back.loan column)
+
+Since both are categorical columns, we will do chi-square test to test the same.
+
+Null Hypothesis : Distribution of purpose across all customers is same.
+
+Alternative Hypothesis : Distribution of purpose for loan defaulters and non defaulters is different.
+If chi-squared statistic exceeds the critical value, reject the null hypothesis that the two distributions are the same, else null hypothesis cannot be rejected
+"""
 #Importing header files
 from scipy.stats import chi2_contingency
 
@@ -108,15 +141,12 @@ from scipy.stats import chi2_contingency
 critical_value = stats.chi2.ppf(q = 0.95, # Find the critical value for 95% confidence*
                       df = 6)   # Df = number of variable categories(in purpose) - 1
 
-#Code starts here
-
 
 # Subsetting the dataframe
 yes=data[data['paid.back.loan']=='Yes']['purpose'].value_counts()
 no=data[data['paid.back.loan']=='No']['purpose'].value_counts()
-print(yes)
-print(no)
-print(yes.transpose())
+
+
 #Concating yes and no into a single dataframe
 observed=pd.concat([yes.transpose(),no.transpose()], 1,keys=['Yes','No'])
 
@@ -133,5 +163,3 @@ print("Chi Statistic")
 print(chi2)
 
 #Code ends here
-
-
