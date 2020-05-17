d50447debf0a4157c22a6eadb483a06f7fd2cd20
Amanjyot62 <Amanjyot62@users.noreply.github.com>
2020-05-17 11:24:22 +0530

    Code file created.

---
+++ Making-Inference-from-Data/code.py
@@ -0,0 +1,137 @@
+# --------------
+import pandas as pd
+import scipy.stats as stats
+import math
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+#Sample_Size
+sample_size=2000
+data=pd.read_csv(path,sep=',')
+#Z_Critical Score
+z_critical = stats.norm.ppf(q = 0.95)  
+data_sample=data.sample(n=sample_size,random_state=0)
+sample_mean=data_sample['installment'].mean()
+sample_std=data_sample['installment'].std()
+
+margin_of_error=z_critical * (sample_std/math.sqrt(sample_size))
+confidence_interval=(sample_mean - margin_of_error, sample_mean + margin_of_error)
+# path        [File location variable]
+true_mean=data['installment'].mean()
+print(true_mean)
+
+#Code starts here
+
+
+
+# --------------
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Different sample sizes to take
+sample_size=np.array([20,50,100])
+
+#Code starts here
+
+#Creating different subplots
+fig,axes=plt.subplots(3,1, figsize=(10,20))
+
+#Running loop to iterate through rows
+for i in range(len(sample_size)):
+    
+    #Initialising a list
+    m=[]
+    
+    #Loop to implement the no. of samples
+    for j in range(1000):
+        
+        #Finding mean of a random sample
+        mean=data['installment'].sample(sample_size[i]).mean()
+        
+        #Appending the mean to the list
+        m.append(mean)
+        
+        
+    #Converting the list to series
+    mean_series=pd.Series(m)   
+
+    #Plotting the histogram for the series
+    axes[i].hist(mean_series, normed=True)
+
+    
+
+#Displaying the plot
+plt.show()
+
+
+#Code ends here
+
+
+# --------------
+#Importing header files
+
+from statsmodels.stats.weightstats import ztest
+x=data['int.rate'].map(lambda x: str(x)[:-1])
+
+x=x.astype(float)/100
+#print(x)
+data['int.rate']=x
+
+z_statistic, p_value = ztest(data[data['purpose']=='small_business']['int.rate'],value=data['int.rate'].mean(),alternative='larger')
+if p_value<0.05:
+    print('Reject')
+else:
+    print("Accept")
+#Code starts here
+
+
+
+# --------------
+#Importing header files
+from statsmodels.stats.weightstats import ztest
+z_statistic,p_value=ztest(data[data['paid.back.loan']=='No']['installment'],data[data['paid.back.loan']=='Yes']['installment'])
+
+if p_value<0.05:
+    print("reject")
+else:
+    print("Accept")    
+#Code starts here
+
+
+
+# --------------
+#Importing header files
+from scipy.stats import chi2_contingency
+
+#Critical value 
+critical_value = stats.chi2.ppf(q = 0.95, # Find the critical value for 95% confidence*
+                      df = 6)   # Df = number of variable categories(in purpose) - 1
+
+#Code starts here
+
+
+# Subsetting the dataframe
+yes=data[data['paid.back.loan']=='Yes']['purpose'].value_counts()
+no=data[data['paid.back.loan']=='No']['purpose'].value_counts()
+print(yes)
+print(no)
+print(yes.transpose())
+#Concating yes and no into a single dataframe
+observed=pd.concat([yes.transpose(),no.transpose()], 1,keys=['Yes','No'])
+
+print(observed)
+
+chi2, p, dof, ex = chi2_contingency(observed)
+
+
+print("Critical value")
+print(critical_value)
+
+
+print("Chi Statistic")
+print(chi2)
+
+#Code ends here
+
+
